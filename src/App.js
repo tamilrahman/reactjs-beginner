37901bbd8040565578bc835fd492960437be04e2
thamizharasan <thamizh@medzdigital.com>
2019-05-03 11:17:51 +0530

    stateless functional component, stateful class component, properties in functional component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+import Hello from './components/Hello'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name= "Name 1" superHero= "Super 1">
+      	<p>Helloo paragraph</p>
+      	<button className="btn">Action</button>
+      </Greet>
+      <Greet name= "Name 2" superHero= "Super 2">
+      	<p> Name 2 paragraph</p>
+      </Greet>
+      <Greet name= "Name 3" superHero= "Super 3"/>
+    	{/* <Welcome /> */}
+    	<Hello name= "Thamizh"> 
+    		<p> sample paragraph </p>
+    	</Hello>
     </div>
   );
 }
---
+++ src/components/Greet.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// function Greet() {
+//  return <h1> Stateless Fuctional Component</h1>
+// }
+const Greet = props => {
+ return (
+ 	<div>
+	 	<h1> hello <i>{props.name}</i> as <i>{props.superHero}</i></h1>
+	 	{ props.children }
+	</div>
+ )
+
+}
+
+export default Greet
