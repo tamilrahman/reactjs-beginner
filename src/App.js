31df39fb8aaafedf55a2dca7662dfb8d83752be9
Thamizharasan Jayakumar <tamilrahmanic@gmail.com>
2019-05-03 15:22:57 +0530

    basics of State in Class component

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello'
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
-      <Greet name= "Name 1" superHero= "Super 1">
-      	<p>Helloo paragraph</p>
-      	<button className="btn">Action</button>
+      <Message />
+     {/* <Greet name= "Name 1" superHero= "Super 1">
+        <p>Helloo paragraph</p>
+        <button className="btn">Action</button>
       </Greet>
       <Greet name= "Name 2" superHero= "Super 2">
-      	<p> Name 2 paragraph</p>
+        <p> Name 2 paragraph</p>
       </Greet>
       <Greet name= "Name 3" superHero= "Super 3"/>
-    	<Hello name= "Thamizh"> 
-    		<p> sample paragraph </p>
-    	</Hello>
-    	<Welcome name= "Class Name 1"></Welcome>
-    	<Welcome name= "Class Name 2"></Welcome>
+      <Hello name= "Thamizh"> 
+        <p> sample paragraph </p>
+      </Hello>
+      <Welcome name= "Class Name 1"></Welcome>
+      <Welcome name= "Class Name 2"></Welcome> */} 
     </div>
   );
 }
